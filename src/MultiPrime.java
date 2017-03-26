27852398af51328991034072d2b285764a0c9162
stefan2212 <stefan.cernescu94@gmail.com>
2017-03-26 17:32:34 +0100

    Implemented key generation

--- src/MultiPrime.java
+++ src/MultiPrime.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Random;
 
 /**
@@ -9,13 +10,22 @@ public class MultiPrime {
     private BigInteger p=new BigInteger("0");
     private BigInteger q=new BigInteger("0");
     private BigInteger r=new BigInteger("0");
+    private BigInteger e=new BigInteger("41");
+    private BigInteger d=new BigInteger("0");
+    private BigInteger phi=new BigInteger("0");
     public static final String ALGORITHM = "RSA";
+    private RSAPublicKey blindingKey = null;
     public MultiPrime(){
         Random rand=new Random();
         p=BigInteger.probablePrime(512,rand);
         q=BigInteger.probablePrime(512,rand);
         r=BigInteger.probablePrime(512,rand);
         n=p.multiply(q.multiply(r));
+        BigInteger pMinus1=p.subtract(new BigInteger("1"));
+        BigInteger qMinus1=q.subtract(new BigInteger("1"));
+        BigInteger rMinus1=r.subtract(new BigInteger("1"));
+        phi=pMinus1.multiply(qMinus1.multiply(rMinus1));
+        d=e.modInverse(phi);
     }
 
     public void doEncryption(){
@@ -23,5 +33,9 @@ public class MultiPrime {
         System.out.println(p);
         System.out.println(q);
         System.out.println(r);
+        System.out.println(e);
+        System.out.println(d);
     }
+
+
 }
